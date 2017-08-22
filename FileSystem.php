6f8273b6d6a667c97867aba8bb0270dbf7da6c49
iglad <aliyousefi.tec@gmail.com>
2017-08-22 14:01:58 +0430

    add copy command
    use in index.php?cmd=copy&src=a&dest=b

--- FileSystem.php
+++ FileSystem.php
@@ -11,14 +11,17 @@ class FileSystem
      */
     public function filterDir($list)
     {
-        if(SHOW_HIDE_FILE)
+        if (SHOW_HIDE_FILE) {
             return $list;
+        }
         $hideList = [];
         foreach ($list as $file) {
-            if($file == '..')
+            if ($file == '..') {
                 continue;
-            if($file[0] == '.')
+            }
+            if ($file[0] == '.') {
                 $hideList[] = $file;
+            }
         }
         return array_diff($list, $hideList);
     }
@@ -29,17 +32,53 @@ class FileSystem
         $cwd = getcwd();
         foreach ($list as $file) {
             $ext = '';
+            $info = '';
             $path = $name = $file;
-            if($file == '..')
-            {
+            if ($file == '..') {
                 $name = '<-- Parent Directory';
             }
-            if(is_dir($file) && $file != '..')
+            if (is_dir($file) && $file != '..') {
                 $name .= '/';
-            
-            echo "<a href=\"index.php?dir=$cwd/$path\" > $name </a>" ;
-            
+            }
+
+            if (is_file($file)) {
+                $info = 'size: ' . $this->human_filesize(filesize($file));
+            }
+
+            echo "<a href=\"index.php?dir=$cwd/$path\" > $name </a>\t" ;
+            echo $info;
             echo  END_LINE;
+        }
+    }
+    
+    /**
+     *
+     * Convert bytes to human readable
+     */
+    public function human_filesize($bytes, $decimals = 2)
+    {
+        $sz = 'BKMGTP';
+        $factor = floor((strlen($bytes) - 1) / 3);
+        if ($bytes < 1024 || ($bytes%1024 == 0)) {
+            return sprintf("%ld", $bytes / pow(1024, $factor)) . @$sz[$factor];
+        }
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+    }
+
+    public function copy($src, $dest)
+    {
+        if(file_exists($dest)){
+            echo 'File: ' .$dest .' before exists, choose another name'. END_LINE;
+            return;
+        }
+        
+        if(copy($src, $dest))
+        {
+            echo "Successful copy $src to $dest " . END_LINE;
+        }
+        else
+        {
+            echo "Faild Copy $src \n" . END_LINE; 
         }
     }
 }
--- index.php
+++ index.php
@@ -7,4 +7,17 @@ if (isset($_GET['dir'])) {
     $dir = $_GET['dir'];
 }
 $fs = new FileSystem($dir);
+
+// Command 
+$cmd = '';
+if (isset($_GET['cmd'])) {
+    $cmd = $_GET['cmd'];
+}
+switch($cmd)
+{
+    case 'copy' :
+        $fs->copy( $_GET['src'],  $_GET['dest']);
+    break;
+}
+
 $fs->showList();
