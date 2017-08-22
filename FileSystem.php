ac0e87e9a3cee1c2bc075a6d1b02c8ebfb0348ce
iglad <aliyousefi.tec@gmail.com>
2017-08-22 10:47:17 +0430

    remove path peroperty and simplified list directory and files

--- FileSystem.php
+++ FileSystem.php
@@ -1,32 +1,17 @@
 <?php
 class FileSystem
 {
-    public $path;
-    public $cwd;
-
     public function __construct($path = '.')
     {
-        //$this->setPath($path);
-        $this->setPath('.');
         chdir($path);
     }
-    public function setPath($path)
-    {
-        $this->path = $path;
-       // $this->cwd  = chdir($path);
-    }
 
-    public function getPath()
-    {
-        return $this->path;
-    }
     public function showList()
     {
-        $list = scandir($this->getPath());
+        $list = scandir('.');
         $cwd = getcwd();
         foreach($list as $file)
         {
-            
             echo "<a href=\"index.php?dir=$cwd/$file\" > $file </a>" . END_LINE;
         }
     }
