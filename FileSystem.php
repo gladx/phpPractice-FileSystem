2046efaba06c82e650983474c014aee9ea3cdddd
iglad <aliyousefi.tec@gmail.com>
2017-08-25 00:10:24 +0430

    add File Permission

--- FileSystem.php
+++ FileSystem.php
@@ -43,6 +43,10 @@ class FileSystem
 
             if (is_file($file)) {
                 $info = 'size: ' . $this->human_filesize(filesize($file));
+                
+            }
+            if ($file != '..') {
+                $info .=  ' ' . $this->getPermission($file);
             }
 
             echo "<a href=\"index.php?dir=$cwd/$path\" > $name </a>\t" ;
@@ -142,5 +146,60 @@ class FileSystem
         } else {
             echo "Move/Rename is Failed ." . END_LINE;
         }
+    }
+
+    // php.net/manual/en/function.fileperms.html example 2
+    public function getPermission($src)
+    {
+        $perms = fileperms($src);
+        
+        switch ($perms & 0xF000) {
+            case 0xC000: // socket
+                $info = 's';
+                break;
+            case 0xA000: // symbolic link
+                $info = 'l';
+                break;
+            case 0x8000: // regular
+                $info = '-'; # r
+                break;
+            case 0x6000: // block special
+                $info = 'b';
+                break;
+            case 0x4000: // directory
+                $info = 'd';
+                break;
+            case 0x2000: // character special
+                $info = 'c';
+                break;
+            case 0x1000: // FIFO pipe
+                $info = 'p';
+                break;
+            default: // unknown
+                $info = 'u';
+        }
+        
+        // Owner
+        $info .= (($perms & 0x0100) ? 'r' : '-');
+        $info .= (($perms & 0x0080) ? 'w' : '-');
+        $info .= (($perms & 0x0040) ?
+                    (($perms & 0x0800) ? 's' : 'x' ) :
+                    (($perms & 0x0800) ? 'S' : '-'));
+        
+        // Group
+        $info .= (($perms & 0x0020) ? 'r' : '-');
+        $info .= (($perms & 0x0010) ? 'w' : '-');
+        $info .= (($perms & 0x0008) ?
+                    (($perms & 0x0400) ? 's' : 'x' ) :
+                    (($perms & 0x0400) ? 'S' : '-'));
+        
+        // World
+        $info .= (($perms & 0x0004) ? 'r' : '-');
+        $info .= (($perms & 0x0002) ? 'w' : '-');
+        $info .= (($perms & 0x0001) ?
+                    (($perms & 0x0200) ? 't' : 'x' ) :
+                    (($perms & 0x0200) ? 'T' : '-'));
+        
+        return $info;
     }
 }
